9fcf46b5523a174b086517abdb0d3b874aed950e
sanjay kumar <palankisanjay@gmail.com>
2025-11-09 10:59:02 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -13,3 +13,33 @@ resource "aws_instance" "catalogue" {
         }
     )
 }
+
+# Connect to instance using remotr-exec provisinor  through terraform_data
+
+resource "terraform_data" "catalogue" {
+  triggers_replace = [
+    aws_instance.catalogue.id
+  ]
+  
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = aws_instance.catalogue.private_ip
+  }
+
+  # terraform copies this file to mongodb server
+  provisioner "file" {
+    source = "bootstrap.sh"
+    destination = "/tmp/catalogue.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "chmod +x /tmp/catalogue.sh",
+        #"sudo sh /tmp/catalogue.sh"
+        "sudo sh /tmp/catalogue.sh catalogue"
+        
+    ]
+  }
+}
