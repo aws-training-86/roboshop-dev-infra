db946e6f43d1554067bf84df00ed6fb2873a6551
sanjay kumar <palankisanjay@gmail.com>
2025-11-21 16:32:02 +0530

    terraform

--- 60-catalogue/data.tf
+++ 60-catalogue/data.tf
@@ -19,9 +19,9 @@ data "aws_ami" "joindevops" {
     }
 }
 
-data "aws_ssm_parameter" "private_subnet_id" {
-  name = "/${var.project_name}/${var.environment}/private_subnet_id"
-}
+# data "aws_ssm_parameter" "private_subnet_id" {
+#   name = "/${var.project_name}/${var.environment}/private_subnet_id"
+# }
 
 data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/roboshop/dev/private_subnet_ids"
