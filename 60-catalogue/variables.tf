6e445179a46a8daa18c2c17b9f85586d60adf5b7
sanjay kumar <palankisanjay@gmail.com>
2025-11-21 15:56:23 +0530

    terraform

---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,32 @@
+# variable "project_name" {
+#     default = "roboshop"
+# }
+
+# variable "environment" {
+#     default = "dev"
+# }
+
+# variable "sg_names" {
+#     default = [
+#         # databases
+#         "mongodb", "redis", "mysql", "rabbitmq",
+#         # backend
+#         "catalogue", "user", "cart", "shipping", "payment",
+#         # frontend
+#         "frontend",
+#         # bastion
+#         "bastion",
+#         # frontend load balancer
+#         "frontend_alb",
+#         # Backend ALB
+#         "backend_alb",
+#     ]
+# }
+
+# variable "zone_id" {
+#     default = "Z10208233EZ8AOLML2P24"
+# }
+
+# variable "domain_name" {
+#     default = "daws86s.space"
+# }
